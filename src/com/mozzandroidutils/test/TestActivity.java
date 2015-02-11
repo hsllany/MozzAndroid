c99edf0bfa20ad237aa0aa288c83efde78c9e99a
hsllany <hsllany@163.com>
2015-02-11 15:25:26 +0800

    adding README.md

--- src/com/mozzandroidutils/test/TestActivity.java
+++ src/com/mozzandroidutils/test/TestActivity.java
@@ -7,11 +7,31 @@ import android.util.Log;
 import com.mozzandroidutils.file.SDCard;
 import com.mozzandroidutils.http.DownloaderHttpUtils;
 import com.mozzandroidutils.http.HttpDownloadListener;
+import com.mozzandroidutils.http.HttpListener;
+import com.mozzandroidutils.http.HttpResponse;
+import com.mozzandroidutils.http.HttpUtils;
 
 public class TestActivity extends Activity {
 	@Override
 	protected void onCreate(Bundle savedInstanceState) {
 		super.onCreate(savedInstanceState);
+
+		HttpUtils httpUtils = new HttpUtils();
+		httpUtils.get("http://www.baidu.com", new HttpListener() {
+
+			@Override
+			public void onSuccess(HttpResponse response) {
+				if (response.status == HTTP_OK)
+					System.out.println(response.html);
+
+			}
+
+			@Override
+			public void onFail(HttpResponse response) {
+				// TODO Auto-generated method stub
+
+			}
+		});
 
 	}
 }
