79917511d2e313f00eaaee8128f37374707b1576
hsllany <hsllany@163.com>
2015-04-20 12:18:18 +0800

    adding increment size to DownloadListener

--- src/com/mozz/http/HttpUtils.java
+++ src/com/mozz/http/HttpUtils.java
@@ -393,7 +393,7 @@ public class HttpUtils {
 					downloadSize += bytes;
 					out.write(buffer, 0, bytes);
 					if (mListener != null) {
-						mListener.onDownloading(downloadSize,
+						mListener.onDownloading(downloadSize, bytes,
 								(float) downloadSize / (float) fileSize);
 					}
 
